c57b7900a76b2bf8de7bebd2664364cfc7d6b010
jammy <920817290@qq.com>
2024-11-06 00:19:45 +0800

    1.加入登录状态服务

--- src/LogicSystem.cpp
+++ src/LogicSystem.cpp
@@ -1,6 +1,7 @@
 #include "LogicSystem.h"
 #include "HttpConnection.h"
 #include "VerifyGrpcClient.h"
+#include "StatusGrpcClient.h"
 #include "RedisMgr.h"
 #include "nlohmann/json.hpp"
 #include "MysqlMgr.h"
@@ -18,7 +19,44 @@ LogicSystem::LogicSystem() {
         beast::ostream(connection->_response.body()) << "receive get_test req";
     });
 
-    RegPost("/get_varifycode", [](std::shared_ptr<HttpConnection> connection) {
+    RegPost("/get_varifycode_register", [](std::shared_ptr<HttpConnection> connection) {
+        auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
+        LOG_INFO("receive body is %s ", body_str.c_str());
+        connection->_response.set(http::field::content_type, "text/json");
+
+        json body_json;
+        json response_json;
+        try{
+            body_json = json::parse(body_str);
+        }catch (json::parse_error& e){
+            LOG_ERROR( "Failed to parse JSON data!");
+             response_json["error_code"] = ERRORCODE::Error_Json;
+             response_json["body"] = body_str;
+            beast::ostream(connection->_response.body()) << response_json;
+            return true;
+        }
+       std::string email = body_json["email"];
+       std::string name = body_json["user"];
+       ///要重置密码的用户不存在
+       if(!MysqlMgr::GetInstance()->checkUserExist(name,email)){
+            LOG_ERROR( "User Or Email Not Exist!");
+            response_json["error_code"] = ERRORCODE::Error_User_Not_Exist;
+            response_json["body"] = body_str;
+            beast::ostream(connection->_response.body()) << response_json;
+            return true;
+        }
+       GetVarifyRsp rsp = VerifyGrpcClient::GetInstance()->GetVarifyCode(email);
+        auto err  = rsp.error();
+       response_json["error_code"] = 0;
+       response_json["email"] = rsp.email();
+       ///验证码是没必要返回给客户端的，这里只是方便调试以及新增用户
+       response_json["code"] = rsp.code();
+    
+       beast::ostream(connection->_response.body()) << response_json.dump();
+       return true;
+    });
+
+    RegPost("/get_varifycode_reset", [](std::shared_ptr<HttpConnection> connection) {
         auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
         LOG_INFO("receive body is %s ", body_str.c_str());
         connection->_response.set(http::field::content_type, "text/json");
@@ -38,26 +76,142 @@ LogicSystem::LogicSystem() {
 
        GetVarifyRsp rsp = VerifyGrpcClient::GetInstance()->GetVarifyCode(email);
         auto err  = rsp.error();
-        LOG_INFO("receive grpc, size = %d, rsp = %s",rsp.DebugString().size(),rsp.DebugString().c_str());
-       LOG_INFO( "email is %s" ,  rsp.email().c_str());
-       LOG_INFO( "code is %s" , rsp.code().c_str());
-        LOG_INFO( "error is %d" , err);
        response_json["error_code"] = 0;
        response_json["email"] = rsp.email();
+       ///验证码是没必要返回给客户端的，这里只是方便调试以及新增用户
        response_json["code"] = rsp.code();
     
        beast::ostream(connection->_response.body()) << response_json.dump();
        return true;
     });
-
     RegPost("/user_register", [](std::shared_ptr<HttpConnection> connection) {
+        auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
+        std::cout << "receive body is " << body_str << std::endl;
+        connection->_response.set(http::field::content_type, "text/json");
+        json body_json;
+        json response_json;
+        try{
+            body_json = json::parse(body_str);
+        }catch(json::parse_error& e){
+            LOG_ERROR( "Failed to parse JSON data!");
+            response_json["error"] = ERRORCODE::Error_Json;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+        std::string email = body_json["email"];
+        std::string name = body_json["user"];
+        std::string pwd = body_json["passwd"];
+        std::string code = body_json["code"];
+        //先查找redis中email对应的验证码是否合理
+        std::string  varify_code;
+        std::string key = REDIS_EMAIL_CODE_PREFIX+email;
+        bool b_get_varify = RedisMgr::GetInstance()->Get(key, varify_code);
+        if (!b_get_varify) {
+            std::cout << " get varify code expired" << std::endl;
+            response_json["error"] = ERRORCODE::Varify_Code_Expired;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+        if (varify_code != code) {
+            std::cout << " varify code error" << std::endl;
+            response_json["error"] = ERRORCODE::Varify_Code_Not_Equal;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+
+        int uid = MysqlMgr::GetInstance()->RegUser(name, email, pwd);
+        if (uid == 0 || uid == -1) {
+            std::cout << " user or email exist" << std::endl;
+            response_json["error"] = ERRORCODE::Error_User_Exist;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+        std::cout << " user register success" << std::endl;
+        response_json["error"] = ERRORCODE::Error_User_Exist;
+        std::string jsonstr = response_json.dump();
+        beast::ostream(connection->_response.body()) << jsonstr;
+        return true;
+    });
+    RegPost("/user_reset_password", [](std::shared_ptr<HttpConnection> connection) {
+        auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
+        std::cout << "receive body is " << body_str << std::endl;
+        connection->_response.set(http::field::content_type, "text/json");
+        json body_json;
+        json response_json;
+        try{
+            body_json = json::parse(body_str);
+        }catch(json::parse_error& e){
+            LOG_ERROR( "Failed to parse JSON data!");
+            response_json["error"] = ERRORCODE::Error_Json;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+        std::string email = body_json["email"];
+        std::string name = body_json["user"];
+        std::string pwd = body_json["passwd"];
+        std::string code = body_json["code"];
+
+        //先查找redis中email对应的验证码是否合理
+        std::string  varify_code;
+        std::string key = REDIS_EMAIL_CODE_PREFIX+email;
+        bool b_get_varify = RedisMgr::GetInstance()->Get(key, varify_code);
+        if (!b_get_varify) {
+            std::cout << " get varify code expired" << std::endl;
+            response_json["error"] = ERRORCODE::Varify_Code_Expired;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+        if (varify_code != code) {
+            std::cout << " varify code error" << std::endl;
+            response_json["error"] = ERRORCODE::Varify_Code_Not_Equal;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+        //查找数据库判断用户是否存在
+        if(MysqlMgr::GetInstance()->checkUserExist(name, email)){
+            std::cout << "user not exist" << std::endl;
+            response_json["error"] = ERRORCODE::Error_User_Not_Exist;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+        
+
+        if(MysqlMgr::GetInstance()->updatePassword(name,email,pwd)){
+            response_json["error"] = 0;
+            response_json["email"] = email;
+            response_json ["user"]= name;
+            response_json["passwd"] = pwd;
+            response_json["code"] = code;
+            std::cout << "update password success" << std::endl;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }else{
+            std::cout << "sql server error update passwd" << std::endl;
+            response_json["error"] = ERRORCODE::Error_Update_Password;
+            std::string jsonstr = response_json.dump();
+            beast::ostream(connection->_response.body()) << jsonstr;
+            return true;
+        }
+
+    });
+
+    RegPost("/user_login", [](std::shared_ptr<HttpConnection> connection) {
     auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
     std::cout << "receive body is " << body_str << std::endl;
     connection->_response.set(http::field::content_type, "text/json");
-    json body_json;
+    json request_json;
     json response_json;
     try{
-        body_json = json::parse(body_str);
+        request_json = json::parse(body_str);
     }catch(json::parse_error& e){
         LOG_ERROR( "Failed to parse JSON data!");
         response_json["error"] = ERRORCODE::Error_Json;
@@ -65,55 +219,38 @@ LogicSystem::LogicSystem() {
         beast::ostream(connection->_response.body()) << jsonstr;
         return true;
     }
-    
-    std::string email = body_json["email"];
-    std::string name = body_json["user"];
-    std::string pwd = body_json["passwd"];
-    std::string confirm = body_json["confirm"];
-    std::string code = body_json["code"];
-    if (pwd != confirm) {
-        std::cout << "password err " << std::endl;
-        response_json["error"] = ERRORCODE::Password_Not_Equal;
+
+    std::string name = request_json["user"];
+    std::string pwd = request_json["passwd"];
+    ///UserInfo userInfo;
+    //查询数据库判断用户名和密码是否匹配
+    UserInfo userInfo;
+    bool pwd_valid = MysqlMgr::GetInstance()->checkUserPassword(name, pwd,userInfo);
+    if (!pwd_valid) {
+        std::cout << " user pwd not match" << std::endl;
+        response_json["error"] = ERRORCODE::ERROR_PASSWORD_NOT_CORRECT;
         std::string jsonstr = response_json.dump();
         beast::ostream(connection->_response.body()) << jsonstr;
         return true;
     }
-    //先查找redis中email对应的验证码是否合理
-    std::string  varify_code;
-    std::string key = REDIS_EMAIL_CODE_PREFIX+email;
-    bool b_get_varify = RedisMgr::GetInstance()->Get(key, varify_code);
-    if (!b_get_varify) {
-        std::cout << " get varify code expired" << std::endl;
-        response_json["error"] = ERRORCODE::Varify_Code_Expired;
+    //查询StatusServer找到合适的连接
+    auto reply = StatusGrpcClient::GetInstance()->GetChatServer(userInfo.uid);
+    if (reply.error()) {
+        std::cout << " grpc get chat server failed, error is " << reply.error()<< std::endl;
+        response_json["error"] = ERRORCODE::RPCFailed;
         std::string jsonstr = response_json.dump();
         beast::ostream(connection->_response.body()) << jsonstr;
         return true;
     }
-    if (varify_code != code) {
-        std::cout << " varify code error" << std::endl;
-        response_json["error"] = ERRORCODE::Varify_Code_Not_Equal;
-        std::string jsonstr = response_json.dump();
-        beast::ostream(connection->_response.body()) << jsonstr;
-        return true;
-    }
-    //查找数据库判断用户是否存在
-    int uid = MysqlMgr::GetInstance()->RegUser(name, email, pwd);
-    if (uid == 0 || uid == -1) {
-        std::cout << " user or email exist" << std::endl;
-        response_json["error"] = ERRORCODE::Error_User_Exist;
-        std::string jsonstr = response_json.dump();
-        beast::ostream(connection->_response.body()) << jsonstr;
-        return true;
-    }
+    std::cout << "succeed to load userinfo uid is " << userInfo.uid << std::endl;
     response_json["error"] = 0;
-    response_json["uid"] = uid;
-    response_json["email"] = email;
-    response_json ["user"]= name;
-    response_json["passwd"] = pwd;
-    response_json["confirm"] = confirm;
-    response_json["code"] = code;
+    response_json["user"] = name;
+    response_json["uid"] = userInfo.uid;
+    response_json["token"] = reply.token();
+    response_json["host"] = reply.host();
     std::string jsonstr = response_json.dump();
     beast::ostream(connection->_response.body()) << jsonstr;
+    std::cout<<jsonstr<<std::endl;
     return true;
     });
 }
