27ae33b8f05feebd04dab572f2f3b8d071d83f50
jammy <920817290@qq.com>
2024-11-06 00:17:48 +0800

    1.添加登录状态管理服务

---
+++ src/StatusGrpcClient.cpp
@@ -0,0 +1,28 @@
+#include "StatusGrpcClient.h"
+
+
+GetChatServerRsp StatusGrpcClient::GetChatServer(int uid)
+{
+    ClientContext context;
+    GetChatServerRsp reply;
+    GetChatServerReq request;
+    request.set_uid(uid);
+    auto stub = pool_->getConnection();
+    Status status = stub->GetChatServer(&context, request, &reply);
+    if (status.ok()) {    
+        pool_->returnConnection(std::move(stub));
+        return reply;
+    }
+    else {
+        reply.set_error(ERRORCODE::RPCFailed);
+        pool_->returnConnection(std::move(stub));
+        return reply;
+    }
+}
+StatusGrpcClient::StatusGrpcClient()
+{
+    
+    std::string host = gConfigMgr["StatusServer"]["Host"];
+    std::string port = gConfigMgr["StatusServer"]["Port"];
+    pool_.reset(new StatusConPool(5, host, port));
+}
